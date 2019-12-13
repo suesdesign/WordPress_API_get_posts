231f99f2346f56e0b9222885669266b46adb2d6c
Sue Johnson <sjohnsondesign@gmail.com>
2019-12-13 21:42:53 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+README.md
+
+# Suesdesign WordPress API Posts
+
+A WordPress plugin to output posts from another WordPress site using the WordPress REST API.
+
+Built using PHP OOP
---
+++ suesdesign_api.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @package Suesdesign API
+ * @version 1.0
+ */
+/*
+Plugin Name: Suesdesign WordPress API Posts
+Plugin URI: http://suesdesign.co.uk/
+Description: Get posts using the WordPress API
+Author: Sue Johnson
+Version: 1.0
+Author URI: http://suesdesign.co.uk/
+*/
+
+class Suesdesign_get_posts
+{
+    // GET the remote site
+    private $url = 'http://localhost/wordpresstest/wp-json/wp/v2/posts';
+    public function __construct() {
+    // Add_shortcode    
+        add_shortcode('get_json', array($this, 'get_json_posts'));
+    }
+    public function get_json_posts() {
+        $this->response = wp_remote_get( $this->url );
+        $posts = json_decode( wp_remote_retrieve_body( $this->response ) );
+        $allposts = '';
+    // Check for error
+        if ( !$posts  ) {
+            return sprintf( '<p>The URL %1s could not be retrieved.</p>', $this->url ); //get just the body
+        } else {
+    // Loop through posts
+        
+        foreach ( $posts as $post ) {
+    // Format the date.
+    		$fordate = date( 'n/j/Y', strtotime( $post->modified ) );
+    // Show a title, post date and content
+    		$allposts .= '<h2>' . $post->title->rendered . '</h2>';
+            $allposts .= '<p>' . $fordate . '</p>';
+    		$allposts .= $post->content->rendered;
+    	}
+    	return $allposts;
+        }
+    }
+}
+
+new Suesdesign_get_posts();
